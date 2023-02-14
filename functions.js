32e148436cf5ec9b521cc0e1f1398ff5a2e04337
Guilherme Augusto Castello Costa <guilherme.castello.clear@gmail.com>
2023-02-14 08:40:57 -0300

    Added Anonymous Functions and Arrow Functions

--- functions.js
+++ functions.js
@@ -23,3 +23,27 @@ function calc(x1, x2, op){// Defining function calc with parameters x1 (value 1)
 }
 
 console.log(calc(2, 2, "-")); //Executing calc function, using "-" as operator.
+
+/*
+    ANONYMOUS FUNCTIONS
+*/
+
+
+console.log((function(x1, x2, op){ //Anonymous functions can be used like this. We don't need to name it.
+
+    return eval(`${x1} ${op} ${x2}`);
+
+})(2, 3, "*"));
+
+/*
+
+    ARROW FUNCTION
+
+*/
+
+let calcular = (x1, x2, op) => { //Functions too can be defined like a variable, as we can see here
+
+    return eval(`${x1} ${op} ${x2}`);
+
+}
+
